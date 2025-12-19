e1b4a8174acd0318c3899da370b0f9271fdaf7d3
Roaa Khaled <131912358+Rowlkh@users.noreply.github.com>
2025-12-19 18:44:26 +0200

    Update Receiver_Code.ino

--- Code/Receiver_Code/Receiver_Code.ino
+++ Code/Receiver_Code/Receiver_Code.ino
@@ -1,33 +1,59 @@
+int sensor = 2;   
+int red = 3;      
+int green = 4;
+int blue = 5;
+
 void setup() {
-  pinMode(3, OUTPUT);
-  pinMode(4, INPUT);
-  pinMode(5, INPUT);
-  pinMode(6, INPUT);
+  pinMode(sensor, INPUT);
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+  
   Serial.begin(9600);
+  Serial.println("RECEIVER READY");
 }
 
 void loop() {
-  // Button 1
-  if (digitalRead(4) == HIGH) {
-    Serial.println("B1");
-    digitalWrite(3, HIGH); delay(100); digitalWrite(3, LOW); delay(100);
-    digitalWrite(3, HIGH); delay(100); digitalWrite(3, LOW); delay(100);
-    digitalWrite(3, HIGH); delay(100); digitalWrite(3, LOW);
-    while(digitalRead(4) == HIGH); // Wait for release
-  }
-  
-  // Button 2
-  if (digitalRead(5) == HIGH) {
-    Serial.println("B2");
-    digitalWrite(3, HIGH); delay(300); digitalWrite(3, LOW); delay(300);
-    digitalWrite(3, HIGH); delay(300); digitalWrite(3, LOW);
-    while(digitalRead(5) == HIGH);
-  }
-  
-  // Button 3
-  if (digitalRead(6) == HIGH) {
-    Serial.println("B3");
-    digitalWrite(3, HIGH); delay(600); digitalWrite(3, LOW);
-    while(digitalRead(6) == HIGH);
+  if (digitalRead(sensor) == LOW) {
+    
+    long t = 0;
+    while (digitalRead(sensor) == LOW) {
+      delay(1);
+      t = t + 1;
+    }
+    
+    // Show measurement
+    Serial.print("Got: ");
+    Serial.print(t);
+    Serial.println(" ms");
+    
+    // Button 1 (100ms) = RED
+    if (t > 80 && t < 120) {
+      Serial.println("Button 1 - RED");
+      digitalWrite(red, HIGH);
+      delay(2000);
+      digitalWrite(red, LOW);
+    }
+    
+    // Button 2 (300ms) = GREEN
+    else if (t > 250 && t < 350) {
+      Serial.println("Button 2 - GREEN");
+      digitalWrite(green, HIGH);
+      delay(2000);
+      digitalWrite(green, LOW);
+    }
+    
+    // Button 3 (600ms) = BLUE
+    else if (t > 550 && t < 650) {
+      Serial.println("Button 3 - BLUE");
+      digitalWrite(blue, HIGH);
+      delay(2000);
+      digitalWrite(blue, LOW);
+    }
+    
+    // Unknown signal
+    else {
+      Serial.println("Unknown signal");
+    }
   }
 }
