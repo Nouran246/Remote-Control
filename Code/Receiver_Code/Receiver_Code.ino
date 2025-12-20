97b336a163fe716c7db60b58259dd5cca180d191
Roaa Khaled <131912358+Rowlkh@users.noreply.github.com>
2025-12-20 12:36:27 +0200

    Update Receiver_Code.ino

--- Code/Receiver_Code/Receiver_Code.ino
+++ Code/Receiver_Code/Receiver_Code.ino
@@ -1,59 +1,40 @@
-int sensor = 2;   
-int red = 3;      
-int green = 4;
-int blue = 5;
-
 void setup() {
-  pinMode(sensor, INPUT);
-  pinMode(red, OUTPUT);
-  pinMode(green, OUTPUT);
-  pinMode(blue, OUTPUT);
-  
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT);  // Red
+  pinMode(4, OUTPUT);  // Green
+  pinMode(5, OUTPUT);  // Blue
   Serial.begin(9600);
-  Serial.println("RECEIVER READY");
+  Serial.println("READY");
 }
 
 void loop() {
-  if (digitalRead(sensor) == LOW) {
+  if (digitalRead(2) == LOW) {
+    unsigned long start = millis();
     
-    long t = 0;
-    while (digitalRead(sensor) == LOW) {
-      delay(1);
-      t = t + 1;
-    }
+    // Wait for signal to end (with timeout)
+    while(digitalRead(2) == LOW && millis() - start < 2000);
     
-    // Show measurement
+    unsigned long pulseTime = millis() - start;
+    
     Serial.print("Got: ");
-    Serial.print(t);
+    Serial.print(pulseTime);
     Serial.println(" ms");
     
-    // Button 1 (100ms) = RED
-    if (t > 80 && t < 120) {
-      Serial.println("Button 1 - RED");
-      digitalWrite(red, HIGH);
-      delay(2000);
-      digitalWrite(red, LOW);
+    // NON-OVERLAPPING ranges:
+    if (pulseTime > 150 && pulseTime < 250) {    // ~200ms
+      Serial.println("B1 - RED");
+      digitalWrite(3, HIGH); delay(2000); digitalWrite(3, LOW);
     }
-    
-    // Button 2 (300ms) = GREEN
-    else if (t > 250 && t < 350) {
-      Serial.println("Button 2 - GREEN");
-      digitalWrite(green, HIGH);
-      delay(2000);
-      digitalWrite(green, LOW);
+    else if (pulseTime > 350 && pulseTime < 450) { // ~400ms
+      Serial.println("B2 - GREEN");
+      digitalWrite(4, HIGH); delay(2000); digitalWrite(4, LOW);
     }
-    
-    // Button 3 (600ms) = BLUE
-    else if (t > 550 && t < 650) {
-      Serial.println("Button 3 - BLUE");
-      digitalWrite(blue, HIGH);
-      delay(2000);
-      digitalWrite(blue, LOW);
+    else if (pulseTime > 550 && pulseTime < 650) { // ~600ms
+      Serial.println("B3 - BLUE");
+      digitalWrite(5, HIGH); delay(2000); digitalWrite(5, LOW);
     }
-    
-    // Unknown signal
     else {
-      Serial.println("Unknown signal");
+      Serial.println("Noise - Ignored");
     }
   }
 }
